2640a8a9a23e0a57d91538d4c3e5d2ddb6c52568
Paulo Baskovic <pbaskovi@gmail.com>
2025-12-27 18:04:36 +0100

    Add menu items for card count and learn functionality

---
+++ __init__.py
@@ -0,0 +1,34 @@
+# import the main window object (mw) from aqt
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo, qconnect
+# import all of the Qt GUI library
+from aqt.qt import *
+
+# We're going to add a menu item below. First we want to create a function to
+# be called when the menu item is activated.
+
+def testFunction() -> None:
+    # get the number of cards in the current collection, which is stored in
+    # the main window
+    cardCount = mw.col.card_count()
+    # show a message box
+    showInfo("Card count: %d" % cardCount)
+
+def learnFunction() -> None:
+    # placeholder for learn button functionality
+    showInfo("Learn button clicked!")
+
+# create a new menu item, "test"
+action = QAction("test", mw)
+# set it to call testFunction when it's clicked
+qconnect(action.triggered, testFunction)
+# and add it to the tools menu
+mw.form.menuTools.addAction(action)
+
+# create a new menu item, "Learn"
+learnAction = QAction("Learn", mw)
+# set it to call learnFunction when it's clicked
+qconnect(learnAction.triggered, learnFunction)
+# and add it to the tools menu
+mw.form.menuTools.addAction(learnAction)
